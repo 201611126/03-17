99e3bb8616cd542eff48681b755a536b5da85355
201611126 <101780359+201611126@users.noreply.github.com>
2022-03-17 13:38:47 +0900

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,20 @@
+print("Hello World")
+print("Mary's cosmetics")
+print("신씨가 소리질렀다. \"도둑이야\".")
+print("\"C:/Windows\"")   
+print("안녕하세요\n만나서\t\t반갑습니다.")
+print("역슬래쉬t 는 탭키를 눌렀을때 만큼 커서 이동")
+print("역슬래쉬n 은 다음 줄로 커서 이동")
+movie_rank=("닥터 스트레인지","스플릿","럭키")
+movie_rank=("닥터 스트레인지","스플릿","럭키","배트맨")
+movie_rank=['닥터 스트레인지','슈퍼맨','스플릿','럭키','배트맨']
+nums = [1, 2, 3, 4, 5]
+print(sum(nums))    
+cook = ["피자", "김밥", "만두", "앙념치킨", "족발", "피자", "김치만두", "쫄면", "소시지", "라면", "팥빙수", "김치전"]
+print(len(cook))
+print(sum(nums)/len(nums))
+print("false 3과 5는 같지않다.")
+print(3 < 5 )
+print("true 3은 5보다 작다.")
+print("true 4는 1보다 크고 5보다 작다.")
+print("true 3과 3은 서로 같고 4와 3은 값이 동일하지 않다.")
