cb65226f1cdb2e4cdd39e4eb87392d884afece2a
201611126 <101780359+201611126@users.noreply.github.com>
2022-03-24 11:38:26 +0900

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,39 @@
+letters = 'python'
+print(letters[0], letters[2])
+# string = "python"
+# print(string[::-1])
+url = "http://sharebook.kr"
+print(url. rindex("kr"))
+str = input("kr을 입력")
+
+# file_name = "보고서.xlsx"
+# file_name.endswith("xlsx")
+# print(file_name.endswith("xlsx"))
+file_name = "2020_보고서.xlsx"
+print(file_name.startswith("2020"))
+# user_score = input("score:")
+# user_score = int(user_score)
+# if 81 < user_score < 100 :
+#     print("A")
+# elif 61 < user_score < 80 :
+#     print("B")
+# elif 41 < user_score < 60 :
+#     print("C")
+# elif 21 < user_score < 40 :
+#     print("D")
+# else:
+#     print("E")
+cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "소시지", "라면", "팥빙수", "김치전"]
+print(len(cook))
+# warn_investment_list = ["Microsoft", "Google", "Naver", "Kakao", "SAMSUNG", "LG"]
+# user = input("종목을 입력하세요")
+# if user in warn_investment_list:
+#     print("투자 경고 종목입니다.")
+# else :
+#     print("투자 경고 종목이 아닙니다.")
+# 리스트 = [100, 200, 300 ]
+# for cost in 리스트:
+#     print(cost+10)
+리스트 = ["SK하이닉스", "삼성전자", "LG전자"]
+for name in 리스트:
+    print(len(name))
