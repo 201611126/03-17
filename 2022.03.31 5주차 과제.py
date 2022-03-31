fab2100044a383263f29ab1b5265cb98e89e9cc0
201611126 <101780359+201611126@users.noreply.github.com>
2022-03-31 10:06:39 +0900

    Add files via upload

---
+++ 2022.03.31 5주차 과제.py
@@ -0,0 +1,33 @@
+# price_list = [32100, 32150, 32000, 32500]
+# for i in range(len(price_list)):
+#  print(i, price_list[i])
+# price_list = [32100, 32150, 32000, 32500]
+# for i in range(1,4):
+#     print(i*10+90, price_list[i])
+# for i in range(2002, 2051, 4):
+#     print(i)
+# for i in range(10):
+#     print(i/10)
+# ticker = "btc_krw"
+# ticker1 = ticker.upper()
+# print(ticker1)
+# file_name = "보고서.xlsx"
+# print(file_name.endswith("xlsx"))
+# a = "hello world"
+# print(a.split())
+# date = "2020-05-01"
+# print(date.split("-"))
+# 상장주식주 = "5,969,782,550"
+# 상장주식주 = 상장주식주.replace(",","")
+# 상장주식주 = int(상장주식주)
+# print(상장주식주, type(상장주식주))
+# a = "hello world"
+# print(a.split())
+# price = ['20180728', 100, 130, 140, 150, 160, 170]
+# print(price[1:])
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(nums[1::2])
+# 리스트 = [3, 100, 23, 44]
+# for i in 리스트:
+#     if i % 3 ==0:
+#         print(i)
